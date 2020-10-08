231efcafb334595197c0db63b873c15b85b857b1
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-10-09 00:00:41 +0300

    sm7250-common: releasetools: Update firmware images
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: I3c2fdc4a5941d3b4e6034f5fa1d2eaa1e158af81

--- releasetools.py
+++ releasetools.py
@@ -30,9 +30,35 @@ def AddImage(info, basename, dest):
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
+def AddImageRadio(info, basename, dest):
+    name = basename
+    data = info.input_zip.read("RADIO/" + basename)
+    common.ZipWriteStr(info.output_zip, name, data)
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
 def OTA_InstallEnd(info):
   info.script.Print("Patching dtbo...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   info.script.Print("Patching vbmeta...")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+
+  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl");
+  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop");
+  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");
+  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg");
+  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp");
+  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp");
+  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/ImageFv");
+  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster");
+  AddImageRadio(info, "multi_image.mbn", "/dev/block/bootdevice/by-name/multiimgoem");
+  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");
+  AddImageRadio(info, "parameter.img", "/dev/block/bootdevice/by-name/parameter");
+  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw");
+  AddImageRadio(info, "splash.img", "/dev/block/bootdevice/by-name/splash");
+  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec");
+  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz");
+  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp");
+  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config");
+  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl");
   return
