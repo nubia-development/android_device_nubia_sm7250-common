f519e0433c803de727b0ea91a92bb12fbcfd8086
Cyborg2017 <wangyang0987654@gmail.com>
2023-07-09 16:03:58 +0800

    sm7250-common: Decommonize nfc related definitions entirely
    * nubia play 5g not equip with nfc
    
    Change-Id: I114833b54352845d598d185069aed341e6ff8133

--- properties.mk
+++ properties.mk
@@ -206,11 +206,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.data.mode=concurrent \
     ro.vendor.use_data_netmgrd=true \
 
-# NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.factory.nfc=true \
-    ro.boot.product.hardware.sku=nfc
-
 # OEM unlocking
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
--- rootdir/bin/init.nfc.sku.sh
+++
@@ -1,8 +0,0 @@
-#!/vendor/bin/sh
-
-if [ -c /dev/sec-nfc ]; then
-    setprop ro.boot.product.hardware.sku nfc
-else
-    setprop ro.boot.product.hardware.sku nfc_empty
-fi
-
