f65ebca09d06002b8bc7ae9cf641886b48310fb9
Cyborg2017 <wangyang0987654@gmail.com>
2023-07-16 13:09:19 +0800

    Revert "sm7250-common: Remove ffbm.sensors.oem from hals.conf"
    This reverts commit e343335b7a1783a90335784d3ea62e51988c2d50.

--- extract-files.sh
+++ extract-files.sh
@@ -70,10 +70,6 @@ fi
 function blob_fixup() {
     case "${1}" in
 
-    vendor/etc/sensors/hals.conf)
-        sed -i "/ffbm.sensors.oem.so/d" "${2}"
-        ;;
-
     # Change soname for fingerprint.default.so
     vendor/lib/hw/fingerprint.lito.so | vendor/lib64/hw/fingerprint.lito.so)
         "${PATCHELF}" --set-soname "fingerprint.lito.so" "${2}"
