676e261d415d280be577fe723e00447e76aa13bf
Davide Garberi <dade.garberi@gmail.com>
2023-07-30 17:11:10 +0200

    sm7250-common: Add udfpshandler
    Change-Id: I589cff0c12e2688463a4a41381e35341832a58da

--- lito.mk
+++ lito.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.nubia
+    android.hardware.biometrics.fingerprint@2.3-service.nubia \
+    libudfpshandler
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
