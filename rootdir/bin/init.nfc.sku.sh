d6bf36cdff83c9351232e69cc7fc2a3ad8a6d1f3
Cyborg2017 <wangyang0987654@gmail.com>
2023-08-15 22:53:17 +0800

    sm7250-common: rootdir: Determine the region through hardware nfc
    Change-Id: I5ec7063c176a8ae30ab5a2c11568ebfc847f99e5

--- rootdir/bin/init.nfc.sku.sh
+++ rootdir/bin/init.nfc.sku.sh
@@ -2,6 +2,8 @@
 
 if [ -c /dev/sec-nfc ]; then
     setprop vendor.qti.nfc.supported true
+    setprop ro.boot.hwc EN
 else
     setprop vendor.qti.nfc.supported false
+    setprop ro.boot.hwc CN
 fi
